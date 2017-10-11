47d26203158641a7ac2fa935514382ed73ddf6cf
Yu Koga <ukoooga@gmail.com>
2017-10-11 23:06:39 +0900

    modified scroll speed, added secret feature:o

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 $(function(){
+  var numberOfPics = 15;
+  var mypics = [];
+  for (var i = 1; i < numberOfPics+1; i++) {
+    mypics.push("images/bg"+[i]+".JPG");
+  }
+
+  // change background-image on very access
+  // var randomNum = Math.floor(Math.random() * mypics.length);
+  // $('.hero-image').attr({
+  //   style: "background-image: url(" +mypics[randomNum]+ ");"
+  // });
+
 	$('a[href^=\\#]').click(function(){
-		var speed = 200;
+		var time = 1200;
 		var href= $(this).attr("href");
 		var target = $(href == "#" || href == "" ? 'html' : href);
 		var position = target.offset().top;
-		$("html, body").animate({scrollTop:position}, speed, "swing");
+		$("html, body").animate({scrollTop:position}, time, "swing");
 		return false;
 	});
+
+  $('.name').click(function(){
+    var randomNum = Math.floor(Math.random() * mypics.length);
+
+    $(".hero-image")
+        .fadeOut(400, function() {
+          //load new image
+            $(".hero-image").attr({
+              style: "background-image: url(" +mypics[randomNum]+ ");"
+            });
+        })
+        .fadeIn(600);
+  })
 });
