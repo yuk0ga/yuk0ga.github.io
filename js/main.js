9269783e0709315a71d518bfe1ad0e7f71130be9
Yu Koga <ukoooga@gmail.com>
2017-10-18 00:19:13 +0900

    applied WP API

--- js/main.js
+++ js/main.js
@@ -62,4 +62,38 @@ $(function(){
         });
      $(".hero-image").animate({ opacity: 1 }, { duration: 1700 });
   })
+
+  var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://public-api.wordpress.com/wp/v2/sites/yukogablog.wordpress.com/posts?per_page=4');
+ourRequest.onload = function() {
+  if (ourRequest.status >= 200 && ourRequest.status < 400) {
+    var data = JSON.parse(ourRequest.responseText);
+    createHTML(data);
+  } else {
+    console.log("We connected to the server, but it returned an error.");
+  }
+};
+
+ourRequest.onerror = function() {
+  console.log("Connection error");
+};
+
+ourRequest.send();
+
+function createHTML(postsData) {
+  var featuredImg = '<img class="mainimg" src="' + postsData[0].featured_media_url + '"/>';
+  var content = "";
+  content += '<h4>' + postsData[0].title.rendered + '</h4>';
+  content += '<p>' + postsData[0].excerpt.rendered + '</p>';
+  $("#featuredImg").html(featuredImg);
+  $("#content").html(content);
+
+  var otherPosts = "";
+  for (i=1; i<postsData.length; i++) {
+    otherPosts += '<h5>' + postsData[i].title.rendered + '</h5>';
+    otherPosts += postsData[i].excerpt.rendered;
+    otherPosts += '<hr>';
+  }
+  $("#recent-posts").html(otherPosts);
+}
 });
